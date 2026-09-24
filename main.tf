59f7607bbc5eb78bfec2680d30e73718e1d562ed
Samir Upare <samirupare26299@gmail.com>
2026-09-24 20:14:39 +0530

    resouirce create

--- main.tf
+++ main.tf
@@ -1,1 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
+  }
+}
 
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "hcp_ec2" {
+  ami           = "ami-0c02fb55956c7d316"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HCP-Terraform-EC2"
+  }
+}
